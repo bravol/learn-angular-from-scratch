c81dfb20fd184a962f78e359abd2a253fb2a14e8
Brian Bravol <lumalabravo@gmail.com>
2024-11-08 11:39:22 +0300

    using signals

--- angular-signals-example/src/app/app.component.ts
+++ angular-signals-example/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, effect } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +6,25 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-signals-example';
+
+  // setting signal
+  theme = signal('light');
+  label = this.theme();
+
+  constructor() {
+    // whenever the signal value changes effect will get executed
+    effect(() => {
+      this.label = this.theme();
+    });
+  }
+
+  toggleDarkMode() {
+    this.theme.update((currentValue) =>
+      currentValue === 'light' ? 'dark' : 'light'
+    );
+  }
 }
